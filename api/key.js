37f5770df74ffe7af532037dcce07253f25b72ef
ashok07-maker <studiousness7@gmail.com>
2025-08-01 10:33:49 +0545

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,16 +1,13 @@
 module.exports = async (req, res) => {
-    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // Handle OPTIONS request
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
     }
 
-    // Return the API key
     const apiKey = process.env.GEMINI_API_KEY;
     console.log('API Key endpoint called - Key exists:', !!apiKey);
     
