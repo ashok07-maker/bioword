f9076e949aeef20db7947b70b66e22502b957200
ashok07-maker <studiousness7@gmail.com>
2025-08-01 10:36:22 +0545

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,10 +1,8 @@
 module.exports = async (req, res) => {
-    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // Handle OPTIONS request
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
